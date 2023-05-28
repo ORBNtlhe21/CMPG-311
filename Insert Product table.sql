3e03e911c13c7a72c12dd86f33d6de4f059dc202
Killer-85 <128693848+Killer-85@users.noreply.github.com>
2023-05-29 01:58:51 +0200

    Add files via upload

---
+++ Insert Product table.sql
@@ -0,0 +1,44 @@
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'BlackLable','Beer',25.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'CastleMilkStout','Lager',25.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'BuffaloWings','ChickenWings',89.90);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'BbqWings','ChickenWings',89.90);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'SweetChilliWings','ChickenWings',89.90);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Skop','SheepHead',75.95);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Jug','Wiskey',65.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Jug','Tequila',65.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Jug','Vodka',65.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Jug','Rum',65.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'SteakMeal','200g',135.95);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'SteakMeal','400g',195.90);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Jagemeister','750ml',515.00);
+
+INSERT INTO PRODUCT 
+VALUES (Seq_Product.NEXTVAL,'Jagemeister','1liter',750.00);
+
+
+
