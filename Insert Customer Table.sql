33cc919baeb2d8869700bbb1bb1ef224dac4b082
Killer-85 <128693848+Killer-85@users.noreply.github.com>
2023-05-29 01:56:59 +0200

    Add files via upload

---
+++ Insert Customer Table.sql
@@ -0,0 +1,44 @@
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Kisha', 'Williams','0872453671','kishaW@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Mike', 'Dudson','0789021453','mikedudson@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Michelle', 'Micheals','0667093217','mmicheals@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Dee', 'Wilson','0719237465','deewilson@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Kat', 'Danielson','0783410293','kat@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Duncan', 'Law','0669030801','dLaw@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Max', 'Steel','0668021712','msteel@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Peter', 'Parker','0667032623','ppaker@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'John', 'Wick','0667033534','jwick@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Jack', 'Sparrow','0667034445','jsparrow@gmail.com');
+
+INSERT INTO CUSTOMER 
+VALUES (Seq_Customer.NEXTVAL,'Kanye', 'West','0667035356','kwest@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Josiah', 'The Desciple','0556035356','jdesciple@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Bongani', 'Mokoena','0456035356','bmokoena@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Boss', 'Paper','0356035356','bpaper@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'George', 'Bush','0256035356','gbush@gmail.com');
