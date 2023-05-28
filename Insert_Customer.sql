d27f5e593687e9af27a9c908191254e1bb504535
SAVAG3SAV1OUR <113189263+SAVAG3SAV1OUR@users.noreply.github.com>
2023-05-28 23:26:07 +0200

    Add files via upload

---
+++ Insert_Customer.sql
@@ -0,0 +1,17 @@
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Kisha', 'Williams','0872453671','kishaW@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Mike', 'Dudson','0789021453','mikedudson@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Michelle', 'Micheals','0667093217','mmicheals@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Dee', 'Wilson','0719237465','deewilson@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Kat', 'Danielson','0783410293','kat@gmail.com');
+
+INSERT INTO CUSTOMER
+VALUES (Seq_Customer.NEXTVAL,'Duncan', 'Law','0669030801','dLaw@gmail.com');
